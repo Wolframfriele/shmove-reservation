84f7b591bbfb8beac01adc293405c41e6f860d38
assignon <yanick007.dev@gmail.com>
2020-11-18 12:08:07 +0100

    api router, django routing set

--- shmove_reservations/urls.py
+++ shmove_reservations/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from .router import router
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+    path('admin/', admin.site.urls), # load admin urls
+    path('api/', include(router.urls)), # api base route
+    # url(r'^$', TemplateView.as_view(template_name='index.html')), # embeded static frontend files
+    # frontend urls
+    # path('', views.home, name='home'),
+
+] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT,
+)
