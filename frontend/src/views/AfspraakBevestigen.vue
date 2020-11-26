26b7689ceaf0efb39ac2c9e916d27aa1a6ce0f0f
Wolfram Friele <wolframfriele@gmail.com>
2020-11-26 20:57:26 +0100

    Calender v01

---
+++ frontend/src/views/AfspraakBevestigen.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-main>
+    <h1 class="display-1">Afspraak Bevestigen:</h1>
+  </v-main>
+  
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+
+</style>
---
+++ frontend/src/views/AfspraakMaken.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-main>
+      <h1 class="display-1">Afspraak Maken:</h1>
+      <TijdKiezen class="calender"/>
+    </v-main>
+</template>
+
+<script>
+import TijdKiezen from '../components/TijdKiezen.vue'
+export default {
+  components: {
+    TijdKiezen
+  } 
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+
+  .calender {
+    width: 90%;
+    margin: auto;
+    overflow: hidden;
+  }
+</style>
