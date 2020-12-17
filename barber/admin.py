af273dea36307fa900db6822d751c8d8a77060a7
Yanick Assignon <assignon>
2020-12-17 02:50:58 +0100

    appointments model updated, get free places and get appointment data functions rewrite

--- barber/admin.py
+++ barber/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Appointments, Barbers, Employees, Worktimes, WorktimeSlices
+from .models import Appointments, Credentials, Daily, Openinghours, Timeslices
 
 # Register your models here.
 admin.site.register(Appointments)
-admin.site.register(Barbers)
-admin.site.register(Employees)
-admin.site.register(Worktimes)
-admin.site.register(WorktimeSlices)
+admin.site.register(Credentials)
+admin.site.register(Daily)
+admin.site.register(Openinghours)
+admin.site.register(Timeslices)
--- shmove_reservations/router.py
+++ shmove_reservations/router.py
@@ -1,7 +1,6 @@
 from rest_framework import routers
-from barber.views import TestView, AppointmentsView  # example of rest api view import
+from barber.views import AppointmentsView  # example of rest api view import
 
 
 router = routers.DefaultRouter()
-router.register('test', TestView)  # example of how to register a function as api route
 router.register('appointments', AppointmentsView)
