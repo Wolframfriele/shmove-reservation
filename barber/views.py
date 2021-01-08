2b94f1fcab51c3df6f686b4e3fbb2bcae8aef46a
Yanick Assignon <assignon>
2021-01-08 12:07:55 +0100

    free places api bug fix

--- barber/views.py
+++ barber/views.py
@@ -1,31 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.shortcuts import render, redirect, get_object_or_404
+from datetime import datetime, timedelta, date
+
 from django.contrib.auth.models import User
-from barber.models import Appointments, Credentials, Changes, StandardWeek, TimeSlices
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.core import serializers
-from django.db.models import Q
-import json
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.dateparse import parse_date, parse_time
+from django.core.mail import send_mail
 # from django.contrib.auth.hashers import make_password
 ################################## DRF IMPORTS #######################################
-from rest_framework import viewsets, permissions
-from rest_framework.decorators import api_view, action, permission_classes
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
+from django.db.models import Q
 
+from barber.models import Appointments, Credentials, Changes, StandardWeek, TimeSlices, Treatments
 from barber.serializers import TestSerializer, AppointmentSerializer
-from django.core.files import File
-from django.conf import settings
-from datetime import datetime, timedelta
 
 
 # Create your views here.
@@ -62,8 +52,14 @@ class AppointmentsView(viewsets.ModelViewSet):
     @csrf_exempt
     @action(methods=['post'], detail=False)
     def new_appointment(self, request):
-        date_booked_start = datetime.strptime(getpost(request, 'date_booked_start'), '%Y-%m-%d %H:%M')
-        date_booked_end = datetime.strptime(getpost(request, 'date_booked_end'), '%Y-%m-%d %H:%M')
+        dbs_string = getpost(request, 'date_booked_start')
+        dbs_strings = dbs_string.split()
+        date_ = dbs_strings[0]
+        dbe_string = getpost(request, 'date_booked_end')
+        dbe_strings = dbe_string.split()
+        time_start = dbs_strings[1]
+        time_end = dbe_strings[1]
+
         treatment = getpost(request, 'treatment')
         reason = getpost(request, 'reason')
 
@@ -72,106 +68,186 @@ class AppointmentsView(viewsets.ModelViewSet):
         email = getpost(request, 'email')
         phone_number = getpost(request, 'phone_number')
 
-        if_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name, email=email,
-                                                 phone_number=phone_number)
-        if if_credentials:
-            # credentials were found
-            make_credentials = if_credentials
-        else:
-            # credentials were not found
-            make_credentials = Credentials.objects.create(first_name=first_name, last_name=last_name, email=email,
-                                                          phone_number=phone_number)
+        valid = 0
 
-        make_appointment = Appointments.objects.create(date_booked_start=date_booked_start,
-                                                       date_booked_end=date_booked_end,
-                                                       treatment=treatment, reason=reason, credentials=make_credentials)
-        return Response({"created": True, "first_name": first_name, "email": email, "phone_number": phone_number,
-                         "date": date_booked_start})
+        if treatment:
+            valid += 1
+        if reason:
+            valid += 1
+        if first_name:
+            valid += 1
+        if last_name:
+            valid += 1
+        if email:
+            valid += 1
+        if phone_number:
+            valid += 1
+
+        if valid == 6:
+            if dbs_string:
+                valid += 1
+            if dbe_string:
+                valid += 1
+            if valid == 8:
+                try:
+                    # credentials were found
+                    make_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
+                                                               email=email, phone_number=phone_number)
+                except:
+                    # credentials were not found
+                    make_credentials = Credentials.objects.create(first_name=first_name, last_name=last_name,
+                                                                  email=email, phone_number=phone_number)
+
+                treatment_ = Treatments.objects.get(pk=treatment)
+                get_slice = TimeSlices.objects.get(slice_start=time_start, slice_end=time_end).pk
+                print('test1')
+                print(make_credentials)
+                # make_appointment = Appointments.objects.create(time_slice_id=get_slice, treatment=treatment_,
+                #                                                reason=reason, credentials=make_credentials, date=date_)
+                print('test2')
+                try:
+                    test_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
+                                                               email=email, phone_number=phone_number)
+                    print('test4')
+                    try:
+                        print(treatment_)
+                        print(reason)
+                        print(test_credentials)
+                        print(date_)
+                        test_appointment = Appointments.objects.filter(Q(treatment=treatment_) & Q(reason=reason) &
+                                                                       Q(credentials=test_credentials) & Q(date=date_) &
+                                                                       Q(time_slice_id=get_slice)).values()
+                        print('test5')
+                        # send_mail(
+                        #     'Afspraak geplant - IN-KI Shiatsu Delft',
+                        #     'Bedankt voor het maken van een afspraak!<br><br>Test voor enters..',
+                        #     'mailman@shiatsu-delft.nl',
+                        #     ['tijmen.simons@gmail.com'],
+                        #     fail_silently=False,
+                        # )
+                        return Response({"error": "None",
+                                         "first_name": first_name,
+                                         "email": email,
+                                         "phone_number": phone_number,
+                                         "date": date_,
+                                         "time_start": time_start,
+                                         "time_end": time_end})
+                    except:
+                        print('test6')
+                        return Response({"error": "Appointment_Failed"})
+                except:
+                    print('test7')
+                    return Response({"error": "Credentials_Failed"})
+            else:
+                return Response({"error": "Empty_Internal_Field"})
+        else:
+            return Response({"error": "Empty_Field"})
 
     @csrf_exempt
     @action(methods=['get'], detail=False)
     def get_free_places(self, request):
-        beginweek = datetime.strptime(getpost(request, 'beginweek'), '%Y-%m-%d')
-        date = beginweek
-        endweek = datetime.strptime(getpost(request, 'endweek'), '%Y-%m-%d')
+        # get variables
+        date_ = parse_date(getpost(request, 'beginweek'))
+        endweek = parse_date(getpost(request, 'endweek'))
         delta = timedelta(days=1)
-        day = 0
-        slicez = []
-        appointment_slices = Appointments.objects.filter(time_slice_id=2).values()
-        print(appointment_slices)
-        # while date <= endweek:
-        #     print(date)
-        #     # try:
-        #     if_changes = Changes.objects.get(date=date)
-        #     print("I found you", if_changes)
-        #     slices = TimeSlices.objects.filter(changes__date=date).values()
-        #     print(slices)
-        #     for f in slices:
-        #         print(f['id'])
-        #
-                # if appointment_slices:
-                #     print("found")
-                # else:
-                #     print("not found")
-            # except:
-            #     # print(":c did not found")
-            #     pass
-            # print(day)
-            # date += delta
-            # day += 1
-        return Response(slicez)
 
-    @csrf_exempt
-    @action(methods=['get'], detail=False)
-    def get_appointments_customer(self, request):
-        dates = []
-        start_day = datetime.strptime(getpost(request, 'start_day'), '%d/%m/%Y, %H:%M:%S')
-        end_day = datetime.strptime(getpost(request, 'end_day'), '%d/%m/%Y, %H:%M:%S')
-        employee_id = getpost(request, 'employee_id')
-        appointments = Appointments.objects.filter(employee_id=employee_id, date_booked_start__gte=start_day,
-                                                   date_booked_start__lte=end_day).values()
-        for appointment in appointments:
-            dates.append({'date_end': appointment['date_booked_end'], 'date_start': appointment['date_booked_start']})
-        return Response(dates)
+        date_timeslices = []
+        # get the day of the week
+        weekday = datetime.now().weekday() + 1
+        today_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f').split()
+        # get today's date
+        today_date = parse_date(today_datetime[0])
+        current_time = parse_time(today_datetime[1])
+        # check if the received date is earlier than today
+        if date_ < today_date:
+            date_ = today_date
+        # loop through all days between the 2 received dates
+        while date_ <= endweek:
+            slice_array = []
+            count = 0
+            # test if there is a entry in the model "Changes"
+            try:
+                if_changes = Changes.objects.get(date=date_).slice_count
+                slice_count = int(if_changes)
+                slices = TimeSlices.objects.filter(changes__date=date_).values()
+            except:
+                # get the timeslices from the standardweek using the day value
+                slices = TimeSlices.objects.filter(standardweek__pk=weekday).values()
+                slice_count = StandardWeek.objects.get(pk=weekday).slice_count
 
-    @csrf_exempt
-    @action(methods=['get'], detail=False)
-    def get_appointments_barber(self, request):
+            # for every timeslice connected to this date, check if there is an appointment affiliated
+            for f in slices:
+                appointment_slices = Appointments.objects.filter(time_slice_id=f['id'], date=date_)
+                if appointment_slices:
+                    # if an appointment is affiliated
+                    count += 1
+                else:
+                    # if no appointment is affiliated
+                    slice_array.append(f['id'])
 
-        start_day = datetime.strptime(getpost(request, 'start_day'), '%Y-%m-%d')
-        end_day = datetime.strptime(getpost(request, 'end_day'), '%Y-%m-%d')
-        barber_id = getpost(request, 'barber_id')
+            # only send back the timeslices when there are less slices on this day than set as "slice_count"
+            if count < slice_count:
+                for i in slice_array:
+                    slice_data = TimeSlices.objects.filter(pk=i).values()
+                    # make sure no past time_slices get sent
+                    if slice_data[0]["slice_start"] > current_time:
+                        date_timeslices.append({"start": "{} {}".format(date_, slice_data[0]["slice_start"]),
+                                                "end": "{} {}".format(date_, slice_data[0]["slice_end"])})
+            date_ += delta
+            weekday += 1
+        return Response(date_timeslices)
 
-        the_info = []
-        employee_ids = []
-        get_ids = Employees.objects.filter(barber_id=barber_id).values()
-        for mini_id in get_ids:
-            employee_ids.append(mini_id['id'])
-        appointments = Appointments.objects.filter(date_booked_start__gte=start_day, date_booked_start__lte=end_day,
-                                                   employee_id__in=employee_ids).values()
-        for appointment in appointments:
-            if appointment['customer_id'] == 0:
-                cust_data = Credentials.objects.get(pk=appointment['id'])
-            else:
-                cust_data = User.objects.get(pk=appointment['customer_id'])
-
-            the_info.append({'date_end': appointment['date_booked_end'],
-                             'date_start': appointment['date_booked_start'],
-                             'done': appointment['done'],
-                             'treatment': appointment['treatment'],
-                             'customer_first_name': cust_data.first_name,
-                             'customer_last_name': cust_data.last_name,
-                             'customer_email': cust_data.email,
-                             # phone_number
-                             'employee_id': appointment['employee_id']
-                             })
-        return Response(the_info)
-
-    @csrf_exempt
-    @action(methods=['get'], detail=False)
-    def get_appointments_barber(self, request):
-
-        return request("Grave Pain")
+    # @csrf_exempt
+    # @action(methods=['get'], detail=False)
+    # def get_appointments_customer(self, request):
+    #     dates = []
+    #     start_day = datetime.strptime(getpost(request, 'start_day'), '%d/%m/%Y, %H:%M:%S')
+    #     end_day = datetime.strptime(getpost(request, 'end_day'), '%d/%m/%Y, %H:%M:%S')
+    #     employee_id = getpost(request, 'employee_id')
+    #     appointments = Appointments.objects.filter(employee_id=employee_id, date_booked_start__gte=start_day,
+    #                                                date_booked_start__lte=end_day).values()
+    #     for appointment in appointments:
+    #         dates.append({'date_end': appointment['date_booked_end'], 'date_start': appointment['date_booked_start']})
+    #     return Response(dates)
+    #
+    # @csrf_exempt
+    # @action(methods=['get'], detail=False)
+    # def get_appointments_barber(self, request):
+    #
+    #     start_day = datetime.strptime(getpost(request, 'start_day'), '%Y-%m-%d')
+    #     end_day = datetime.strptime(getpost(request, 'end_day'), '%Y-%m-%d')
+    #     barber_id = getpost(request, 'barber_id')
+    #
+    #     the_info = []
+    #     employee_ids = []
+    #     get_ids = Employees.objects.filter(barber_id=barber_id).values()
+    #     for mini_id in get_ids:
+    #         employee_ids.append(mini_id['id'])
+    #     appointments = Appointments.objects.filter(date_booked_start__gte=start_day, date_booked_start__lte=end_day,
+    #                                                employee_id__in=employee_ids).values()
+    #     for appointment in appointments:
+    #         if appointment['customer_id'] == 0:
+    #             cust_data = Credentials.objects.get(pk=appointment['id'])
+    #         else:
+    #             cust_data = User.objects.get(pk=appointment['customer_id'])
+    #
+    #         the_info.append({'date_end': appointment['date_booked_end'],
+    #                          'date_start': appointment['date_booked_start'],
+    #                          'done': appointment['done'],
+    #                          'treatment': appointment['treatment'],
+    #                          'customer_first_name': cust_data.first_name,
+    #                          'customer_last_name': cust_data.last_name,
+    #                          'customer_email': cust_data.email,
+    #                          # phone_number
+    #                          'employee_id': appointment['employee_id']
+    #                          })
+    #     return Response(the_info)
+    #
+    # @csrf_exempt
+    # @action(methods=['get'], detail=False)
+    # def get_appointments_barber(self, request):
+    #
+    #     return request("Grave Pain")
 
 # def new_barber():
 #     name = "Shmoving Test"
