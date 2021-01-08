c51331b3e6b59b3d7ea6aa0a8151f7d52be73ebe
Tijmen <tijmen.simons@student.hu.nl>
2021-01-08 13:53:10 +0100

    new_appointments is done

--- barber/views.py
+++ barber/views.py
@@ -17,6 +17,9 @@ from django.db.models import Q
 from barber.models import Appointments, Credentials, Changes, StandardWeek, TimeSlices, Treatments
 from barber.serializers import TestSerializer, AppointmentSerializer
 
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # Create your views here.
 
@@ -89,55 +92,87 @@ class AppointmentsView(viewsets.ModelViewSet):
             if dbe_string:
                 valid += 1
             if valid == 8:
-                try:
-                    # credentials were found
-                    make_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
-                                                               email=email, phone_number=phone_number)
-                except:
-                    # credentials were not found
-                    make_credentials = Credentials.objects.create(first_name=first_name, last_name=last_name,
-                                                                  email=email, phone_number=phone_number)
-
-                treatment_ = Treatments.objects.get(pk=treatment)
                 get_slice = TimeSlices.objects.get(slice_start=time_start, slice_end=time_end).pk
-                print('test1')
-                print(make_credentials)
-                # make_appointment = Appointments.objects.create(time_slice_id=get_slice, treatment=treatment_,
-                #                                                reason=reason, credentials=make_credentials, date=date_)
-                print('test2')
-                try:
-                    test_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
-                                                               email=email, phone_number=phone_number)
-                    print('test4')
+                find_appointment = Appointments.objects.filter(date=date_, time_slice_id=get_slice).values()
+                if find_appointment:
+                    return Response({"error": "Appointment_Taken"})
+                else:
                     try:
-                        print(treatment_)
-                        print(reason)
-                        print(test_credentials)
-                        print(date_)
-                        test_appointment = Appointments.objects.filter(Q(treatment=treatment_) & Q(reason=reason) &
-                                                                       Q(credentials=test_credentials) & Q(date=date_) &
-                                                                       Q(time_slice_id=get_slice)).values()
-                        print('test5')
-                        # send_mail(
-                        #     'Afspraak geplant - IN-KI Shiatsu Delft',
-                        #     'Bedankt voor het maken van een afspraak!<br><br>Test voor enters..',
-                        #     'mailman@shiatsu-delft.nl',
-                        #     ['tijmen.simons@gmail.com'],
-                        #     fail_silently=False,
-                        # )
-                        return Response({"error": "None",
-                                         "first_name": first_name,
-                                         "email": email,
-                                         "phone_number": phone_number,
-                                         "date": date_,
-                                         "time_start": time_start,
-                                         "time_end": time_end})
+                        # credentials were found
+                        make_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
+                                                                   email=email, phone_number=phone_number)
                     except:
-                        print('test6')
-                        return Response({"error": "Appointment_Failed"})
-                except:
-                    print('test7')
-                    return Response({"error": "Credentials_Failed"})
+                        # credentials were not found
+                        make_credentials = Credentials.objects.create(first_name=first_name, last_name=last_name,
+                                                                      email=email, phone_number=phone_number)
+
+                    treatment_ = Treatments.objects.get(pk=treatment)
+                    make_appointment = Appointments.objects.create(time_slice_id=get_slice, treatment=treatment_,
+                                                                   reason=reason, credentials=make_credentials,
+                                                                   date=date_)
+                    try:
+                        test_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
+                                                                   email=email, phone_number=phone_number)
+                        try:
+                            test_appointment = Appointments.objects.get(Q(treatment=treatment_) & Q(reason=reason) &
+                                                                        Q(credentials=test_credentials) &
+                                                                        Q(date=date_) & Q(time_slice_id=get_slice))
+                            real_date = datetime.strptime(dbs_string, '%Y-%m-%d %H:%M:%S').strftime("%d %b, %Y")
+                            # sending a confirmation mail
+                            port = 465
+                            password = 'donthackme1'
+                            smtp_server = 'smtp.gmail.com'
+                            sender_email = 'trojo.mailtesting@gmail.com'
+                            receiver_email = email
+
+                            message = MIMEMultipart("alternative")
+                            message["Subject"] = "Bevestiging Afspraak - IN-KI Shiatsu Delft"
+                            message["From"] = sender_email
+                            message["To"] = receiver_email
+
+                            # Create the plain-text and HTML version of your message
+                            text = """\
+                            Bedankt voor het maken van een afspraak bij IN-KI Shiatsu Delft, {}.<br><br>
+                                De afspraak is op {} om {}.<br>
+                                U heeft gekozen voor {}.<br>
+                                Met de reden '{}'.
+                            """.format(first_name, real_date, time_start, treatment_, reason)
+                            html = """\
+                            <html><body><p>
+                                Bedankt voor het maken van een afspraak bij IN-KI Shiatsu Delft, {}.<br><br>
+                                De afspraak is op {} om {}.<br>
+                                U heeft gekozen voor {}.<br>
+                                Met de reden '{}'.
+                            </p></body></html>
+                            """.format(first_name, real_date, time_start, treatment_, reason)
+
+                            # Turn these into plain/html MIMEText objects
+                            part1 = MIMEText(text, "plain")
+                            part2 = MIMEText(html, "html")
+
+                            # Add HTML/plain-text parts to MIMEMultipart message
+                            # The email client will try to render the last part first
+                            message.attach(part1)
+                            message.attach(part2)
+
+                            # Create a secure SSL context
+                            context = ssl.create_default_context()
+
+                            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                                server.login(sender_email, password)
+                                server.sendmail(sender_email, receiver_email, message.as_string())
+
+                            return Response({"error": "None",
+                                             "first_name": first_name,
+                                             "email": email,
+                                             "phone_number": phone_number,
+                                             "date": date_,
+                                             "time_start": time_start,
+                                             "time_end": time_end})
+                        except:
+                            return Response({"error": "Appointment_Failed"})
+                    except:
+                        return Response({"error": "Credentials_Failed"})
             else:
                 return Response({"error": "Empty_Internal_Field"})
         else:
