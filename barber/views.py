07ba10bf17e304aacabcbc0c9f9f2cfdd377e345
Yanick Assignon <assignon>
2021-01-09 13:35:58 +0100

    new_appointment api try and except, date format issues fixed

--- barber/views.py
+++ barber/views.py
@@ -43,13 +43,23 @@ class AppointmentsView(viewsets.ModelViewSet):
     @csrf_exempt
     @action(methods=['post'], detail=False)
     def new_appointment(self, request):
+        treatment_arr = []
         dbs_string = getpost(request, 'date_booked_start')
-        dbs_strings = dbs_string.split()
-        date_ = dbs_strings[0]
+        dbs_strings = dbs_string.split(',')
+        # => de datum komt vanaf de frontend op deze format: %d/%m/%Y
+        # => en de Db heeft deze format %Y-%m-%d nodig. vandaar de onderstaan wijzigingen
+        # => convert date to datetinme object
+        date_obj = datetime.strptime(dbs_strings[0], '%d/%m/%Y').date()
+        # => get date and change format
+        date_ = date_obj.strftime('%Y-%m-%d')
         dbe_string = getpost(request, 'date_booked_end')
-        dbe_strings = dbe_string.split()
-        time_start = dbs_strings[1]
-        time_end = dbe_strings[1]
+        dbe_strings = dbe_string.split(',')
+        # => remove leading whitespace in splited daet time
+        time_start = dbs_strings[1].strip()
+        # => remove leading whitespace in splited daet time
+        time_end = dbe_strings[1].strip()
+
+        # return Response('hallo')
 
         treatment = getpost(request, 'treatment')
         reason = getpost(request, 'reason')
@@ -96,39 +106,37 @@ class AppointmentsView(viewsets.ModelViewSet):
                         make_credentials = Credentials.objects.create(first_name=first_name, last_name=last_name,
                                                                       email=email, phone_number=phone_number)
 
-                    treatment_ = Treatments.objects.get(pk=treatment)
+                    treatment_ = Treatments.objects.get(treatment=treatment[0])
                     make_appointment = Appointments.objects.create(time_slice_id=get_slice, treatment=treatment_,
                                                                    reason=reason, credentials=make_credentials,
                                                                    date=date_)
-                    try:
-                        test_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
-                                                                   email=email, phone_number=phone_number)
-                        try:
-                            test_appointment = Appointments.objects.get(Q(treatment=treatment_) & Q(reason=reason) &
-                                                                        Q(credentials=test_credentials) &
-                                                                        Q(date=date_) & Q(time_slice_id=get_slice))
-                            real_date = datetime.strptime(
-                                dbs_string, '%Y-%m-%d %H:%M:%S').strftime("%d %b, %Y")
-                            # sending a confirmation mail
-                            port = 465
-                            password = 'donthackme1'
-                            smtp_server = 'smtp.gmail.com'
-                            sender_email = 'trojo.mailtesting@gmail.com'
-                            receiver_email = email
 
-                            message = MIMEMultipart("alternative")
-                            message["Subject"] = "Bevestiging Afspraak - IN-KI Shiatsu Delft"
-                            message["From"] = sender_email
-                            message["To"] = receiver_email
+                    # => heb test_appointment en test_credentials om te checken of het toevoegen goed gegaan is of niet eruit gehaald
+                    # => je zou een try en except bij Appointments.objects.create met een exception InternalError
+                    # => om te checken of het toevoeging goed ging
+                    # => meer info: https://docs.djangoproject.com/en/3.1/ref/exceptions/
+                    real_date = datetime.strptime(
+                        dbs_string, '%d/%m/%Y, %H:%M:%S').strftime("%d %b, %Y")
+                    # sending a confirmation mail
+                    port = 465
+                    password = 'donthackme1'
+                    smtp_server = 'smtp.gmail.com'
+                    sender_email = 'trojo.mailtesting@gmail.com'
+                    receiver_email = email
 
-                            # Create the plain-text and HTML version of your message
-                            text = """\
+                    message = MIMEMultipart("alternative")
+                    message["Subject"] = "Bevestiging Afspraak - IN-KI Shiatsu Delft"
+                    message["From"] = sender_email
+                    message["To"] = receiver_email
+
+                    # Create the plain-text and HTML version of your message
+                    text = """\
                             Bedankt voor het maken van een afspraak bij IN-KI Shiatsu Delft, {}.<br><br>
                                 De afspraak is op {} om {}.<br>
                                 U heeft gekozen voor {}.<br>
                                 Met de reden '{}'.
                             """.format(first_name, real_date, time_start, treatment_, reason)
-                            html = """\
+                    html = """\
                             <html><body><p>
                                 Bedankt voor het maken van een afspraak bij IN-KI Shiatsu Delft, {}.<br><br>
                                 De afspraak is op {} om {}.<br>
@@ -137,34 +145,34 @@ class AppointmentsView(viewsets.ModelViewSet):
                             </p></body></html>
                             """.format(first_name, real_date, time_start, treatment_, reason)
 
-                            # Turn these into plain/html MIMEText objects
-                            part1 = MIMEText(text, "plain")
-                            part2 = MIMEText(html, "html")
+                    # Turn these into plain/html MIMEText objects
+                    part1 = MIMEText(text, "plain")
+                    part2 = MIMEText(html, "html")
 
-                            # Add HTML/plain-text parts to MIMEMultipart message
-                            # The email client will try to render the last part first
-                            message.attach(part1)
-                            message.attach(part2)
+                    # Add HTML/plain-text parts to MIMEMultipart message
+                    # The email client will try to render the last part first
+                    message.attach(part1)
+                    message.attach(part2)
 
-                            # Create a secure SSL context
-                            context = ssl.create_default_context()
+                    # Create a secure SSL context
+                    context = ssl.create_default_context()
 
-                            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-                                server.login(sender_email, password)
-                                server.sendmail(
-                                    sender_email, receiver_email, message.as_string())
+                    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                        server.login(sender_email, password)
+                        server.sendmail(
+                            sender_email, receiver_email, message.as_string())
 
-                            return Response({"error": "None",
-                                             "first_name": first_name,
-                                             "email": email,
-                                             "phone_number": phone_number,
-                                             "date": date_,
-                                             "time_start": time_start,
-                                             "time_end": time_end})
-                        except:
-                            return Response({"error": "Appointment_Failed"})
-                    except:
-                        return Response({"error": "Credentials_Failed"})
+                    return Response({"error": "None",
+                                     "first_name": first_name,
+                                     "email": email,
+                                     "phone_number": phone_number,
+                                     "date": date_,
+                                     "time_start": time_start,
+                                     "time_end": time_end})
+                    # try:
+                    #     test_credentials = Credentials.objects.get(first_name=first_name, last_name=last_name,
+                    # except:
+                    #     return Response({"error": "Credentials_Failed"})
             else:
                 return Response({"error": "Empty_Internal_Field"})
         else:
