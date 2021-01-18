c2e9b85d45565607ee84dba05dea78a4bbea4e53
Liam <Liambenschop@live.nl>
2021-01-18 12:03:06 +0100

    ignore

--- barber/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Appointments, Credentials, Changes, StandardWeek, TimeSlices, Treatments
-
-admin.site.register(Appointments)
-admin.site.register(Credentials)
-admin.site.register(Changes)
-admin.site.register(StandardWeek)
-admin.site.register(TimeSlices)
-admin.site.register(Treatments)
