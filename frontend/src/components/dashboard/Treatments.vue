dfa4b64129b58651a42475582be8a85e3970ca66
Liam <Liambenschop@live.nl>
2021-01-19 11:43:17 +0100

    Treatments API Implementation
    Add/update/delete katching

--- frontend/src/components/dashboard/Treatments.vue
+++ frontend/src/components/dashboard/Treatments.vue
@@ -1,41 +1,51 @@
 <template>
   <v-container class="treatmentContainer">
     <h2>Behandelingen wijzigen</h2>
-    <v-row v-for="(treatment, counter) in treatments" v-bind:key="counter">
+    <v-row v-for="treatment in treatments" v-bind:key="treatment.id">
       <v-col cols="2">
-        <div v-if="treatment.title">
-          <v-subheader>{{ treatment.title }}</v-subheader>
+        <div v-if="treatment.treatment">
+          <v-subheader>{{ treatment.treatment }}</v-subheader>
         </div>
         <div v-else>
           <v-subheader>Nieuwe behandeling</v-subheader>
         </div>
       </v-col>
       <v-col cols="10" class="textfieldContainer">
+        <form @submit.prevent="saveTreatment">
         <v-text-field
           class="textfield"
           label="Naam"
-          :value="treatment.title"
+          :value="treatment.treatment"
+          v-model="treatment.treatment"
         ></v-text-field>
         <v-text-field
           class="textfield price"
           label="Prijs"
           :value="treatment.price"
+          v-model="treatment.price"
           :prefix="currency"
+          type="number"
         ></v-text-field>
-        <div v-if="treatment.title">
-          <v-btn class="mx-2 minus" fab dark x-small color="red" @click="deleteTreatment(counter)">
+        <div v-if="treatment.treatment">
+          <v-btn class="mx-2 minus" fab dark x-small color="red lighten-2" @click="deleteTreatment(treatment.id)">
             <v-icon dark>
               mdi-minus
             </v-icon>
           </v-btn>
-        </div>
-        <div v-else>
-          <v-btn class="mx-2 minus" fab dark x-small color="green" @click="saveTreatment">
+          <v-btn class="mx-2 save" fab dark x-small color="green lighten-2" @click="saveTreatment(treatment.treatment, treatment.price)" >
             <v-icon dark>
               mdi-check
             </v-icon>
           </v-btn>
         </div>
+        <div v-else>
+          <v-btn class="mx-2 minus" fab dark x-small color="green lighten-2" @click="saveTreatment(treatment.treatment, treatment.price)">
+            <v-icon dark>
+              mdi-check
+            </v-icon>
+          </v-btn>
+        </div>
+        </form>
       </v-col>
     </v-row>
     <v-btn class="mx-2 plus" fab dark color="indigo" @click="addTreatment">
@@ -47,49 +57,84 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data: () => ({
     currency: "€",
-    treatments: [
-      {
-        title: "Massage",
-        icon: "mdi-calendar",
-        price: "100.00"
-      },
-      {
-        title: "Voedingscoaching",
-        icon: "mdi-calendar",
-        price: "50.00"
-      }
-    ]
+    treatments: [],
+    prijs: "",
+    behandeling: ""
   }),
+  created() {
+    axios.get(`${this.$store.state.HOST}/api/dashboard/get_treatments/`)
+    .then(res=>{
+      res.data.forEach(element => {
+        this.treatments.push(element)
+      })
+    })
+  },
   methods: {
     addTreatment() {
       this.treatments.push({
-        title: "",
-        icon: "mdi-calendar",
-        duration: "",
+        treatment: "",
         price: ""
       });
     },
-    deleteTreatment(counter) {
-      this.treatments.splice(counter, 1);
-    },
-    saveTreatment(){
+    deleteTreatment(id) {
+      axios
+        .delete(`${this.$store.state.HOST}/api/dashboard/delete_treatments/`, {
+          params: {
+            id: id
+          },
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json"
+            //"Authorization: token ${payload.auth},
+            //"X-CSRFToken": payload.csrftoken,
+          }
+        })
+        .then(res => {
+          //Perform Success Action
+          console.log(res.data);
+          window.location.reload();
+        })
+        .catch(error => {
+          console.log(error);
+        });
 
+    },
+    saveTreatment(treatment, price){
+      let self = this;
+      let body = {
+        name: treatment,
+        price: price
+      };
+      axios
+        .put(`${self.$store.state.HOST}/api/dashboard/update_treatments/`, {
+          body: body,
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json"
+            //"Authorization: token ${payload.auth},
+            //"X-CSRFToken": payload.csrftoken,
+          }
+        })
+        .then(res => {
+          //Perform Success Action
+          console.log(res.data);
+          window.location.reload();
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
     }
-  }
 };
 </script>
 <style scoped>
 .plus {
   position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-}
-.minus {
-  position: absolute;
-  bottom: 25px;
+  left: 25%;
 }
 h2 {
   margin-bottom: 20px;
@@ -110,5 +155,8 @@ h2 {
 .price,
 .duration {
   width: 100px;
+}
+form {
+  display: flex;
 }
 </style>
