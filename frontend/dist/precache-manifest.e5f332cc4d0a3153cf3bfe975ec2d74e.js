a402444417c5bbe199a05fd4e0e6417054acb480
Wolfram Friele <wolframfriele@gmail.com>
2021-03-02 22:40:33 +0100

    Add missing /dist folder

---
+++ frontend/dist/precache-manifest.e5f332cc4d0a3153cf3bfe975ec2d74e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d8f6672f2e684283500235c584033c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b4fbc03b40ada8843e27",
+    "url": "/static/css/about.0f76909e.css"
+  },
+  {
+    "revision": "d6dcb837cbfd89d181e4",
+    "url": "/static/css/app.17ed1e1e.css"
+  },
+  {
+    "revision": "4889eb46d4bfb21a02f6",
+    "url": "/static/css/chunk-vendors.968f76e6.css"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/static/fonts/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/static/fonts/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/static/fonts/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/static/fonts/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/static/fonts/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/static/fonts/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/static/fonts/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/static/fonts/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/static/fonts/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/static/fonts/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/static/fonts/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/static/fonts/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/static/img/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/static/img/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/static/img/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "b4fbc03b40ada8843e27",
+    "url": "/static/js/about.d0d0e6a1.js"
+  },
+  {
+    "revision": "d6dcb837cbfd89d181e4",
+    "url": "/static/js/app.a5541f9a.js"
+  },
+  {
+    "revision": "4889eb46d4bfb21a02f6",
+    "url": "/static/js/chunk-vendors.18636e48.js"
+  }
+]);
